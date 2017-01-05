a57d2efdfab2fec207d5d21c16ae947d5d6c1c64
Agop Shirinian <agop@triller.co>
2017-01-04 21:14:01 -0500

    Split up clue.h into multiple headers

---
+++ h/clue/gles.h
@@ -0,0 +1,6 @@
+#ifndef CLUE_GLES_H
+#define CLUE_GLES_H
+
+#include <OpenGLES/ES2/gl.h>
+
+#endif
---
+++ h/clue/hooks.h
@@ -0,0 +1,34 @@
+#ifndef CLUE_HOOKS_H
+#define CLUE_HOOKS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Required hooks. */
+void clue_hook_setup(void);
+void clue_hook_loaded(void);
+void clue_hook_update(double deltaTime);
+void clue_hook_render(void);
+void clue_hook_teardown(void);
+
+typedef void (*clue_hook_t)(void);
+
+/* Optional hooks. If desired, assign during clue_hook_setup(). */
+extern clue_hook_t clue_hook_will_become_inactive;
+extern clue_hook_t clue_hook_did_become_inactive;
+extern clue_hook_t clue_hook_will_become_active;
+extern clue_hook_t clue_hook_did_become_active;
+extern clue_hook_t clue_hook_will_terminate;
+
+/* iOS-specific hooks. */
+extern clue_hook_t clue_ios_hook_memory_warning;
+
+#define CLUE_HOOK_INVOKE(hook, ...) do { if (clue_hook_##hook) clue_hook_##hook(__VA_ARGS__); } while (false)
+#define CLUE_HOOK_DEFINE(hook, impl) do { clue_hook_##hook = impl; } while (false)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
