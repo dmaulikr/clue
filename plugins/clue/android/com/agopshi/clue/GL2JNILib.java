c383d295721f43644286c1d24efcd0d6600cc39b
Agop Shirinian <agop@triller.co>
2017-01-27 10:39:50 -0500

    Initial working Android project (no clue integration quite yet)

---
+++ plugins/clue/android/com/agopshi/clue/GL2JNILib.java
@@ -0,0 +1,15 @@
+package com.agopshi.clue;
+
+public class GL2JNILib {
+
+     static {
+         System.loadLibrary("gl2jni");
+     }
+
+    /**
+     * @param width the current view width
+     * @param height the current view height
+     */
+     public static native void init(int width, int height);
+     public static native void step();
+}
